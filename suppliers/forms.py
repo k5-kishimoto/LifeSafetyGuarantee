afc3cf2cbe799138b1eefa96dee8a2c6820fd068
k5-kishimoto <k.k.camille.vidan@gmail.com>
2026-01-22 09:58:01 +0900

    文言変更

--- suppliers/forms.py
+++ suppliers/forms.py
@@ -5,9 +5,9 @@ from django.forms import PasswordInput
 
 class AgencySignUpForm(forms.Form):
     # Name is used as Username in Salesforce
-    username = forms.CharField(label='ユーザー名 (ログインID)', max_length=150, required=True)
-    password1 = forms.CharField(label='パスワード', widget=PasswordInput, required=True)
-    password2 = forms.CharField(label='パスワード（確認用）', widget=PasswordInput, required=True)
+    username = forms.CharField(label='会社メールアドレス(以後ログインIDとなります)', max_length=150, required=True)
+    password1 = forms.CharField(label='新規パスワードを設定いたします', widget=PasswordInput, required=True)
+    password2 = forms.CharField(label='上記パスワードをご登録ください（確認用）', widget=PasswordInput, required=True)
     
     # 💡 業者名表示用フィールド (SalesforceのAgencyName__cに相当する情報など)
     agency_name = forms.CharField(label='不動産管理会社名', max_length=100, required=True) 
